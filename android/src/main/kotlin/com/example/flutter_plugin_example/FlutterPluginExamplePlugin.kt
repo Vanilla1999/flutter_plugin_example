7e731a517b55527edd3f2ae06258f5ed80becbf9
zavadskiy_va <zavadskiy_va>
2024-02-17 20:16:54 +0300

    init

---
+++ android/src/main/kotlin/com/example/flutter_plugin_example/FlutterPluginExamplePlugin.kt
@@ -0,0 +1,51 @@
+package com.example.flutter_plugin_example
+
+import androidx.annotation.NonNull
+
+import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler
+import io.flutter.plugin.common.MethodChannel.Result
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.util.Timer
+import kotlin.concurrent.timer
+
+/** FlutterPluginExamplePlugin */
+class FlutterPluginExamplePlugin: FlutterPlugin, MethodCallHandler {
+  /// The MethodChannel that will the communication between Flutter and native Android
+  ///
+  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+  /// when the Flutter Engine is detached from the Activity
+  private lateinit var channel : MethodChannel
+  private var timer: Timer? = null
+  private val scope = CoroutineScope(Dispatchers.Main)
+
+  override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "flutter_plugin_example")
+    channel.setMethodCallHandler(this)
+    var number = 0
+    timer = timer(initialDelay = 100L, period = 1000L ) {
+      number +=1
+      scope.launch {
+        channel.invokeMethod("timer",number)
+      }
+    }
+  }
+
+  override fun onMethodCall(call: MethodCall, result: Result) {
+    if (call.method == "getPlatformVersion") {
+      result.success("Android ${android.os.Build.VERSION.RELEASE}")
+    } else {
+      result.notImplemented()
+    }
+  }
+
+  override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+    channel.setMethodCallHandler(null)
+    timer?.cancel()
+    timer = null
+  }
+}
---
+++ lib/flutter_plugin_example.dart
@@ -0,0 +1,14 @@
+
+import 'flutter_plugin_example_platform_interface.dart';
+
+
+class FlutterPluginExample {
+  Future<String?> getPlatformVersion() {
+    return FlutterPluginExamplePlatform.instance.getPlatformVersion();
+  }
+  Stream<int> get timerStream => FlutterPluginExamplePlatform.instance.timerStream;
+
+  Future<void> init() async {
+    FlutterPluginExamplePlatform.instance.init();
+  }
+}
